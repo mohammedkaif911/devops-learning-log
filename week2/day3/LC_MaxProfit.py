207400a1560defee7fe80bf4f82b04883a91c72f
mohammedkaif911 <mohammedkaif91129@gmail.com>
2026-07-29 13:22:19 +0000

    solved 2 leetcode problems

---
+++ week2/day3/LC_MaxProfit.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        min_price = float('inf')
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price- min_price > profit:
+                profit = price - min_price
+        return profit
